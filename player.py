5c5e7949e5e525a1b07136bd9d0904de46c3732f
DinavHel <gonzalezgomez.adriangg@gmail.com>
2024-12-25 13:48:48 +0100

    Add rate limit to ship shooting

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
 
     def triangle(self):
@@ -28,6 +29,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer -= dt
 
         if keys[pygame.K_a]:
            self.rotate(-dt)
@@ -38,7 +40,9 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.timer <= -0.3:
+                self.shoot()
+                self.timer = 0
 
 
     def move(self, dt):
