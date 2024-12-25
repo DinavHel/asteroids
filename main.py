2eae04e631eaae4af152cfcb5133b1d2d892c0cf
DinavHel <gonzalezgomez.adriangg@gmail.com>
2024-12-25 12:07:51 +0100

    Add player and give it the ability to rotate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from time import sleep
+from player import Player
 
 def main():
     pygame.init()
@@ -8,12 +9,15 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
 
         # Limit the framerate to 60 FPS
